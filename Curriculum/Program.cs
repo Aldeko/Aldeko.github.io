2849a99758b15c4340422ab3e6b26cfb66b01d5d
Unai Aldekozea <unaizeni@hotmail.com>
2018-11-19 14:04:27 +0100

    First commit

---
+++ Curriculum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Curriculum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
